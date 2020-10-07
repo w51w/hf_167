0d1a1dbfd8988ae24e9c593cc08c77710ee91783
cjc <w51w@naver.com>
2020-10-07 22:35:38 +0900

    1007 sync

--- src/main/java/com/spring/biz/store/StoreMenuDTO.java
+++ src/main/java/com/spring/biz/store/StoreMenuDTO.java
@@ -3,12 +3,22 @@ package com.spring.biz.store;
 import java.util.ArrayList;
 
 public class StoreMenuDTO {
+	private String store_name;
 	private int type;
 	private String menubar;
 	private String food;
 	private int food_price;
+	private String food_img;
+	private String food_opt;
 	public ArrayList<StoreMenuDTO> child;
 	
+	
+	public String getStore_name() {
+		return store_name;
+	}
+	public void setStore_name(String store_name) {
+		this.store_name = store_name;
+	}
 	public int getType() {
 		return type;
 	}
@@ -33,6 +43,22 @@ public class StoreMenuDTO {
 	public void setFood_price(int food_price) {
 		this.food_price = food_price;
 	}
-	
+	public String getFood_img() {
+		return food_img;
+	}
+	public void setFood_img(String food_img) {
+		this.food_img = food_img;
+	}
+	public String getFood_opt() {
+		return food_opt;
+	}
+	public void setFood_opt(String food_opt) {
+		this.food_opt = food_opt;
+	}
+	@Override
+	public String toString() {
+		return "StoreMenuDTO [type=" + type + ", menubar=" + menubar + ", food=" + food + ", food_price=" + food_price
+				+ ", food_img=" + food_img + ", child=" + child + "]";
+	}
 	
 }
--- src/main/java/com/spring/biz/store/impl/StoreDAO.java
+++ src/main/java/com/spring/biz/store/impl/StoreDAO.java
@@ -21,10 +21,8 @@ public class StoreDAO {
 	private ResultSet rs = null;
 	
 	private final String GET_STORE_LIST = "select * from store where category_code=?";
-	private final String GET_STORE_MENU_HEAD =
-			"select type,menubar,food,food_price from menu where store_name = ? and type = 0";
-	private final String GET_STORE_MENU_CHILD =
-			"select type,menubar,food,food_price from menu where store_name = ? and type = 1 and menubar = ?";
+	private final String GET_STORE_MENU_HEAD ="select * from menu where store_name = ? and type = 0";
+	private final String GET_STORE_MENU_CHILD ="select * from menu where store_name = ? and menubar = ? and type = 1";
 	
 	public StoreListDTO getStoreList_client(int code) {
 		StoreListDTO storeListDTO = new StoreListDTO();
@@ -63,7 +61,7 @@ public class StoreDAO {
 	public StoreMenuListDTO getMenu_client(String store_name) {
 		StoreMenuListDTO list = new StoreMenuListDTO();
 		 ArrayList<StoreMenuDTO> data = new ArrayList<StoreMenuDTO>();
-		  List<String> menubar = new ArrayList<String>(); 
+		  ArrayList<String> menubar = new ArrayList<String>(); 
 		try {
 			conn = JDBCUtil.getConnection();
 			pstmt = conn.prepareStatement(GET_STORE_MENU_HEAD); 
@@ -71,26 +69,31 @@ public class StoreDAO {
 			rs = pstmt.executeQuery();
 			while(rs.next()) {			//store_name=?, type=0 메뉴바 검색 후 저장
 				menubar.add(rs.getString("menubar"));
-			}	
+			}
 			for(int i =0; i<menubar.size(); i++) { //메뉴바의 개수만큼 쿼리문 실행
-				StoreMenuDTO temp1 = new StoreMenuDTO();
-				StoreMenuDTO temp2 = new StoreMenuDTO();
+				StoreMenuDTO temp1 = new StoreMenuDTO();				
 				temp1.child = new ArrayList<StoreMenuDTO>();
 				pstmt = conn.prepareStatement(GET_STORE_MENU_CHILD);
 				pstmt.setString(1, store_name);
-				pstmt.setString(2, menubar.get(i)); 
+				pstmt.setString(2, menubar.get(i));
 				rs = pstmt.executeQuery();
 				while(rs.next()) {			//store_name=?, type=1, menubar=? 자식 검색 후 저장
+					StoreMenuDTO temp2 = new StoreMenuDTO();
+					temp2.setStore_name(rs.getString("store_name"));
 					temp2.setType(rs.getInt("type"));
 					temp2.setMenubar(rs.getString("menubar"));
 					temp2.setFood(rs.getString("food"));
 					temp2.setFood_price(rs.getInt("food_price"));
+					temp2.setFood_img(rs.getString("food_img"));
+					temp2.setFood_opt(rs.getString("food_opt"));
 					temp1.child.add(temp2);
+					System.out.println(temp1.toString());
 				}
 				temp1.setType(0);
 				temp1.setMenubar(menubar.get(i));
 				data.add(temp1);		//메뉴바가 하나 이상이면 for 문 안에서 add 시켜야 됨
 			}
+			System.out.println(data.toString());
 			list.setList(data);
 		}
 		catch(Exception e) {
